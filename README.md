b3d47c156cabf68cb6ed93032c1ed02b0f6fbc22
Nate Brennand <natebrennand@gmail.com>
2013-02-09 10:35:49 -0500

    Filepicker.io functional. Photo template prepared.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-caturday
+Caturday
 ==========
 
 Because what day isn't better with cats?
+
+Locates faces in the phot you upload and replaces them with cat heads. 
 
 A DevFest 2013 project.
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, request, redirect
 from credentials import filepicker
+from random import random
 
 app = Flask(__name__)
 
@@ -10,13 +11,21 @@ def devfest():
 
 @app.route('/upload')
 def upload():
+	random_id = int(random() * 10**6)
 	return render_template('upload.html',
-			filekey = filepicker)
+		filekey = filepicker,
+		id = random_id)
+
+@app.route('/photo')
+@app.route('/photo/<photo_id>')
+def photo( photo_id = 'FvnGx5agRwCEIWnat6W9'):
+	return render_template('photo.html',
+		url = photo_id)
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
-
 
 if __name__ == '__main__':
 	app.debug = True
